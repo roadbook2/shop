f1416fc5e3d659dc18c9746f16bdb034ced58236
byeungoo <dksekfldks65@naver.com>
2021-07-14 22:30:08 +0900

    쇼핑몰 프로젝트 init

---
+++ src/main/java/com/shop/ShopApplication.java
@@ -0,0 +1,13 @@
+package com.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+}
