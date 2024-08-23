f3d7914bf2dc2aa0fca52607446c990e5c69f6e5
mklinkj <96992046+mklinkj@users.noreply.github.com>
2024-08-23 11:42:36 +0900

    failureHandler와 exceptionHandling의 동작 확인

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.shop.config;
 import com.shop.service.MemberService;
 
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -34,14 +36,24 @@ public class SecurityConfig {
                         .defaultSuccessUrl("/")
                         .usernameParameter("email")
                         .failureUrl("/members/login/error")
+                        .failureHandler(((request,  response, exception) -> {
+                            // 특별한 처리 실패했을 때, "/members/login/error"로 리다이렉트만 하는 동작만 필요하다면  
+                            // failureHandler() 설정 필요없이, failureUrl() 설정만으로도 충분함. 
+                            log.info("### failureHandler ###: {}", exception.getMessage());
+                            response.sendRedirect("/members/login/error");
+                         }))
                 ).logout( logoutCustomizer -> logoutCustomizer
                         .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
                         .logoutSuccessUrl("/")
 
                 ).exceptionHandling(exceptionHandlingCustomizer-> exceptionHandlingCustomizer.authenticationEntryPoint((request, response, authException) -> {
                     if ("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))) {
+                        log.info("### 보호된 리소스 접근 - ajax ### ");
                         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
                     } else {
+                        // 일반 HTTP 접근 요청은 formLogin() 설정에서 처리되서 항상 로그인 페이지로 가기 때문에
+                        // 이 분기 자체가 필요가 없을 것 같긴하다.
+                        log.info("### 보호된 리소스 접근 - no ajax ### ");
                         response.sendRedirect("/members/login/error");
                     }})
                 ).build();
